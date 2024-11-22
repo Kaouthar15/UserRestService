0ce8db604a570117284c1283f4839c567d5015ef
kaoutar <elazamikaoutar@gmail.com>
2024-11-22 10:59:27 +0100

    uservice intreface done

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,5 +1,13 @@
 package service;
 
-public interface UserService {
+import model.Response;
+import model.User;
 
+public interface UserService {
+	
+	Response addUser(User u);
+	Response deleteUser(Long id);
+	User getAllUsers();
+	User getUserById(Long id);
+	
 }
