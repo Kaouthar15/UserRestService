b2e63ff6f58b20090fe188394a23b34bee7beffd
kaoutar <elazamikaoutar@gmail.com>
2024-11-22 10:52:31 +0100

    database connexion'

--- src/main/java/util/Connection.java
+++ src/main/java/util/Connection.java
@@ -1,5 +1,21 @@
 package util;
 
-public class Connection {
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+public class Connection {
+	public static SessionFactory factory;
+	
+	private Connection() {
+		
+	}
+	
+	public static synchronized SessionFactory getSessionFactory() {
+		if (factory == null) {
+			factory = new Configuration().configure().buildSessionFactory();
+		}
+		System.out.println("connexion done");
+		return factory;
+	}
+	
 }
