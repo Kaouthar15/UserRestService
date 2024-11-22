613fbf95be640e7193fde8045e4c84e5226f4722
kaoutar <elazamikaoutar@gmail.com>
2024-11-22 11:20:55 +0100

    api app done

--- src/main/java/app/APIApp.java
+++ src/main/java/app/APIApp.java
@@ -1,5 +1,19 @@
 package app;
 
-public class APIApp {
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.ws.rs.core.Application;
+
+public class APIApp extends Application {
+	private Set<Object> singletons;
+	
+	public APIApp() {
+		singletons = new HashSet<Object>();
+	}
+	
+	@Override
+	public Set<Object> getSingletons(){
+		return singletons;
+	}
 }
